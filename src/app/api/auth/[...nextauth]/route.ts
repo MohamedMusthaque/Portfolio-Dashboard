9be09f88ca9099bae823da71a20e2a618596c0db
MusthaqueAllionTech <musthaqueh@alliontechnologies.com>
2025-11-05 22:15:43 +0530

    APIs updated

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,43 +6,57 @@ import { compare } from "bcrypt";
 const prisma = new PrismaClient();
 
 export const authOptions: AuthOptions = {
-  session: {
-    strategy: "jwt", // Use JWT 
-  },
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials.password) {
-          return null;
-        }
+    session: {
+        strategy: "jwt", // Use JWT 
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (token && session.user) {
+                session.user.id = token.id as string;
+            }
+            return session;
+        },
+    },
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials.password) {
+                    return null;
+                }
 
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email },
+                });
 
-        if (!user) {
-          return null; // User not found
-        }
+                if (!user) {
+                    return null; // User not found
+                }
 
-        const isPasswordValid = await compare(credentials.password, user.password);
+                const isPasswordValid = await compare(credentials.password, user.password);
 
-        if (!isPasswordValid) {
-          return null; // Invalid password
-        }
+                if (!isPasswordValid) {
+                    return null; // Invalid password
+                }
 
-        // Return user object if login is successful
-        return {
-          id: user.id,
-          email: user.email,
-        };
-      },
-    }),
-  ],
+                // Return user object if login is successful
+                return {
+                    id: user.id,
+                    email: user.email,
+                };
+            },
+        }),
+    ],
 };
 
 const handler = NextAuth(authOptions);
